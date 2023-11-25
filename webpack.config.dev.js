1e3f31b6b5b5eef2c8096606d640dbef61e00bbf
Bryan Espina <bryanlenier@gmail.com>
2023-11-25 10:35:01 -0500

    WebPack Server Error

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,11 +3,11 @@ const path = require("path");
 //Para trabajar con html
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 //Para trabajar con Css
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 //Para trabajar con Archivos
-const CopyPlugin = require ('copy-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 //Para trabajar Con las Variables de entorno
-const Dotenv = require('dotenv-webpack');
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   //Para comprimir todo los js
@@ -15,19 +15,19 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js",
-    assetModuleFilename: 'assets/images/[hash][ext][query]'
+    assetModuleFilename: "assets/images/[hash][ext][query]",
   },
-  mode:'development',
+  mode: "development",
   //Para compilar cuando estoy programando
   //watch:true,
   resolve: {
     extensions: [".js"],
-    alias:{
-      '@utils':path.resolve(__dirname,'src/utils/'),
-      '@templates':path.resolve(__dirname,'src/templates/'),
-      '@styles':path.resolve(__dirname,'src/styles/'),
-      '@images':path.resolve(__dirname,'src/assets/images/')
-    }
+    alias: {
+      "@utils": path.resolve(__dirname, "src/utils/"),
+      "@templates": path.resolve(__dirname, "src/templates/"),
+      "@styles": path.resolve(__dirname, "src/styles/"),
+      "@images": path.resolve(__dirname, "src/assets/images/"),
+    },
   },
   //Necesario para usar babel
   module: {
@@ -37,40 +37,44 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
-        }
+        },
       },
       {
         test: /\.css$/i,
-        use:[MiniCssExtractPlugin.loader,
-          'css-loader'
-        ],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.png/,
-        type:'asset/resource'
-      }
-    ]
+        type: "asset/resource",
+      },
+    ],
   },
   plugins: [
     //Para el plugin de html
     new HtmlWebpackPlugin({
-      inject:true,
-      template:'./public/index.html',
-      filename:'./index.html'
+      inject: true,
+      template: "./public/index.html",
+      filename: "./index.html",
     }),
     //Para el plugin de css
     new MiniCssExtractPlugin({
-      filename:'assets/[name].[contenthash].css'
+      filename: "assets/[name].[contenthash].css",
     }),
     //Para el plugin de Archivos
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname,"src","assets/images"),
-          to:"assets/images"
-        }
-      ]
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images",
+        },
+      ],
     }),
-    new Dotenv()
-  ]
+    new Dotenv(),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname,'dist'),
+    compress: true,
+    historyApiFallback: true,
+    port: 3006,
+  },
 }
